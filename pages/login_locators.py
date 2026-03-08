e0e885c51baf0e0d114ec46566caf48546c384f9
Ana Amante <ana.s.amante@gmail.com>
2026-03-08 20:12:38 +0100

    Invalid Login passing

--- pages/login_locators.py
+++ pages/login_locators.py
@@ -2,3 +2,4 @@ class LoginLocators:
     LOGIN_EMAIL = "[data-qa='login-email']"
     LOGIN_PASSWORD = "[data-qa='login-password']"
     LOGIN_BUTTON = "[data-qa='login-button']"
+    LOGIN_ERROR = ".login-form p"
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,6 @@
 from pages.login_page import LoginPage
 from pages.nav_bar_locators import NavBarLocators
+from pages.login_locators import LoginLocators
 from core.config import Config
 
 def test_valid_login(page):
@@ -7,3 +8,9 @@ def test_valid_login(page):
     login_page.login(Config.TEST_EMAIL, Config.TEST_PASSWORD)
     assert "automationexercise.com" in page.url
     assert page.locator(NavBarLocators.LOGOUT_LINK).is_visible()
+
+
+def test_invalid_login(page):
+    login_page = LoginPage(page)
+    login_page.login("invalid@email.com", "invalidpassword")
+    assert "Your email or password is incorrect!" in login_page.get_text(LoginLocators.LOGIN_ERROR)
